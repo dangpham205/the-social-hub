d0e3d5ad9e9cee60b94a01f5fddc837c6b932a4b
haidang <haidawng205@gmail.com>
2023-03-06 22:04:15 +0700

    push

---
+++ db/models/post.py
@@ -0,0 +1,22 @@
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, SmallInteger, BigInteger, JSON, Text, Boolean
+from db import Base
+from sqlalchemy.dialects.mysql import BIGINT, TINYINT
+from cores.models.base_model import BaseModel
+from enums.db import model_enum, table_name_enum
+
+
+class Post(BaseModel):
+    __tablename__ = table_name_enum.POST
+    __table_args__ = {
+        'mysql_engine': 'InnoDB',
+        'mysql_charset': 'utf8mb4'
+    }
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    content = Column(Text, nullable=False)
+    picture = Column(String(20), nullable=True)
+    user = relationship('User', backref='user', 
+                          primaryjoin="and_(User.id==Post.user_id, User.deleted_at == None)")
+    
